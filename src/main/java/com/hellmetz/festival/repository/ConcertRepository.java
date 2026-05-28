18edc13ff3ba38b070f46f7d738d8cc11e24dd91
LK7136 <lounes.kissoum1@gmail.com>
2026-05-28 14:04:03 +0200

    -parti concert fini

---
+++ src/main/java/com/hellmetz/festival/repository/ConcertRepository.java
@@ -0,0 +1,12 @@
+package com.hellmetz.festival.repository;
+
+import com.hellmetz.festival.model.Concert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+// JpaRepository<T, ID> : T = votre entité, ID = type de sa clé primaire
+public interface ConcertRepository extends JpaRepository<Concert, Long> {
+    List<Concert> findAllByOrderByNomGroupeAsc();
+}
